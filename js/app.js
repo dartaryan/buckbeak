3990d1355dc0e1bed6b7dc06bff9a46f8b3ba57e
Ben Akiva <dartaryan@gmail.com>
2021-09-10 01:46:58 +0300

    work on the js file

---
+++ js/app.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+  let item, title, author, publisher, booLink, bookImg;
+  let outputList = document.getElementsById("list-output");
+  let bookURL = "https://www.googleapis.com/books/v1/volumes?q=search+terms";
+  let placeholder = '<img src="https://via.placeholder.com/150">';
+  let searchData;
+
+  //Listener for the search button
+
+  $("#search").click(function())
+});
