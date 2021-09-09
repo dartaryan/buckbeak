1b1601de74e55927cc647059d529a9f673bbcae6
Ben Akiva <dartaryan@gmail.com>
2021-09-10 02:35:05 +0300

    handle error displaying empty search box

--- js/app.js
+++ js/app.js
@@ -24,7 +24,7 @@ $(document).ready(function () {
           } else {
             $("#title").animate({ "margin-top": "10px" }, 1000);
             $(".book-list").css("visibilty", "visible");
-            // displayResults(res);
+            displayResults(res);
           }
         },
         error: function () {
@@ -36,8 +36,71 @@ $(document).ready(function () {
   });
   function displayResults(res) {
     for (let i = 0; i < 6; i += 2) {
-      item = res.items[i];
+      item = response.items[i];
+      title1 = item.volumeInfo.title;
+      author1 = item.volumeInfo.authors;
+      publisher1 = item.volumeInfo.publisher;
+      bookLink1 = item.volumeInfo.previewLink;
+      bookIsbn = item.volumeInfo.industryIdentifiers[1].identifier;
+      bookImg1 = item.volumeInfo.imageLinks
+        ? item.volumeInfo.imageLinks.thumbnail
+        : placeholder;
 
+      item2 = response.items[i + 1];
+      title2 = item2.volumeInfo.title;
+      author2 = item2.volumeInfo.authors;
+      publisher2 = item2.volumeInfo.publisher;
+      bookLink2 = item2.volumeInfo.previewLink;
+      bookIsbn2 = item2.volumeInfo.industryIdentifiers[1].identifier;
+      bookImg2 = item2.volumeInfo.imageLinks
+        ? item2.volumeInfo.imageLinks.thumbnail
+        : placeholder;
+
+      // send output to outputList
+      outputList.innerHTML +=
+        '<div class="row mt-4">' +
+        formatOutput(
+          bookImg1,
+          title1,
+          author1,
+          publisher1,
+          bookLink1,
+          bookIsbn
+        ) +
+        formatOutput(
+          bookImg2,
+          title2,
+          author2,
+          publisher2,
+          bookLink2,
+          bookIsbn2
+        ) +
+        "</div>";
     }
   }
+
+  function formatOutput(bookImg, title, author, publisher, bookLink, bookIsbn) {
+    let viewerURL = "book.html?isbn=" + bookIsbn;
+    let htmlCard = `<div class="col-lg-6">
+                        <div class="row no-gutters">
+                        <div class="col-md-4">
+                        <img src="${bookImg}" class="card-img" alt="...">
+                        </div>
+                        <div class="col-md-8">
+                        <div class="card-body">
+                        <h5 class=card-title">${title}</h5>
+                        <p class="card-text">Author: ${author}</p>
+                        <p class="card-text">Publisher: ${publisher}</p>
+                        <a target="_blank" href="${viewerURL}" class="btn btn-secondary">Read me</a>
+                        </div>
+                        </div>
+                        </div>
+                        </div>
+                        </div>
+                        `;
+    return htmlCard;
+  }
+
+  //handle error displaying empty search box
+
 });
