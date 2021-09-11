49e57b6eb2a3ddf7ce5fbb933e4781540077fb95
Ben Akiva <dartaryan@gmail.com>
2021-09-11 21:33:23 +0300

    finish final design and edits

--- js/index.js
+++ js/index.js
@@ -1,10 +1,13 @@
 const searchButton = document.getElementById("search");
-const placeHolder = '<img src="https://via.placeholder.com/150">';
+const placeHolder = "img/no-cover.png";
 const outputList = document.getElementById("list-output");
 const booklist = document.querySelector("#book-list");
-const errorImageUrl =
-  "https://cdn.pixabay.com/photo/2017/06/08/17/32/not-found-2384304_960_720.jpghttps://cdn.pixabay.com/photo/2017/06/08/17/32/not-found-2384304_960_720.jpg";
-// 'https://cdn.pixabay.com/photo/2017/03/09/12/31/error-2129569_960_720.jpg';
+const errorImage = "img/no-cover.png";
+
+document.addEventListener("DOMContentLoaded", function () {
+  var elems = document.querySelectorAll(".collapsible");
+  var instances = M.Collapsible.init(elems, options);
+});
 
 searchButton.addEventListener("click", () => {
   let searchValue = document.querySelector("input");
@@ -21,7 +24,7 @@ searchButton.addEventListener("click", () => {
         })
         .catch(() => {
           booklist.style.display = "contents";
-          outputList.innerHTML = "Something went wrong...";
+          outputList.innerHTML = `<div class="row"><h2 class="header center grey-text text-lighten-1">Something went wrong...</h2></div>`;
         });
     });
   }
@@ -29,17 +32,24 @@ searchButton.addEventListener("click", () => {
 
 function nothingFound() {
   booklist.style.display = "contents";
-  outputList.innerHTML = `<div class="bookCard">
-  <div id="block_container">
-    <div class="image_div">
-    <img id="nothingImage" src="img/nothing.gif" alt="Nothing Found"/>
-  </div>
-
-    <div class="card-content">
-      <h2 class="bookTitle">Nothing found if nothing searched</h2>
-      <p>Maybe try typing something next time to get the best books results Buckbeak can offer</p>
+  outputList.innerHTML = `<div class="col s12 m12 l12">
+  <div class="card horizontal" style="background-color:rgb(197, 194, 194);">
+    <div class="card-image">
+      <img class="nothing-img" src="img/nothing.gif" alt="Nothing Found" >
     </div>
-</div>;`;
+    <div class="card-stacked">
+      <div class="card-content center">
+        <h4 class="card-title grey-text text-darken-4">Nothing found if nothing searched</h4>
+        <div class="card-action">
+          <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley" class="grey-text text-darken-4" >
+            <h6>
+            Maybe try typing something next time to get the best books results Buckbeak can offer</h6>
+          </a>
+        </div>
+      </div>
+    </div>
+  </div>
+  </div>`;
 }
 
 function displayResults(data) {
@@ -57,13 +67,53 @@ function displayResults(data) {
       ? book.volumeInfo.imageLinks.thumbnail
       : placeHolder;
     booklist.style.display = "contents";
-    outputList.innerHTML +=
-      "<div>" + formatOutput(img, title, author, description) + "</div>";
+    outputList.innerHTML += formatOutput(img, title, author, description);
   });
-  handleError();
+  // handleError();
 }
 
 function formatOutput(bookImg, bookTitle, bookAuthor, bookDescription) {
+  let htmlCard = `<div class="col s12 m12 l6">
+  <div class="card horizontal" style="background-color:rgb(197, 194, 194);">
+    <div class="card-image">
+      <img class="card-img" src="${bookImg}" alt="${bookTitle}" >
+    </div>
+    <div class="card-stacked">
+      <div class="card-content center">
+        <h4 class="card-title grey-text text-darken-4">${bookTitle}</h4>
+        <div class="card-action">
+          <a href="https://www.google.com/search?q=${bookAuthor}" class="grey-text text-darken-4" >
+            <h6>
+            ${bookAuthor}</h6>
+          </a>
+        </div>
+        <a class="grey-text text-darken-4 waves-effect waves-light btn-small yellow darken-3 activator">More info</a>
+      </div>
+    </div>
+    <div class="card-reveal">
+      <span class="card-title">${bookTitle}<i class="right">X</i></span>
+      <p>${bookDescription}</p>
+    </div>
+  </div>
+  </div>`;
+
+  return htmlCard;
+}
+
+const handleError = () => {
+  let images = document.querySelectorAll(".card-image");
+  images.forEach((image) => {
+    image.onerror = () => {
+      image.setAttribute("src", errorImage);
+      image.nextSibling.nodeValue = "";
+      image.classList.add("brokenImage");
+    };
+  });
+};
+
+{
+  /* 
+ function formatOutput(bookImg, bookTitle, bookAuthor, bookDescription) {
   let htmlCard = `<div class="card horizontal">
   <div id="block_container">
     <div class="image_div">
@@ -77,18 +127,19 @@ function formatOutput(bookImg, bookTitle, bookAuthor, bookDescription) {
       <h3>${bookAuthor}</h3>
       </div>
     </div>
-</div>;`;
-
-  return htmlCard;
+</div>;`;  */
 }
 
-const handleError = () => {
-  let images = document.querySelectorAll(".cardImage");
-  images.forEach((image) => {
-    image.onerror = () => {
-      image.setAttribute("src", errorImageUrl);
-      image.nextSibling.nodeValue = "";
-      image.classList.add("brokenImage");
-    };
-  });
-};
+// function nothingFound() {
+//   booklist.style.display = "contents";
+//   outputList.innerHTML = `<div class="bookCard">
+//   <div id="block_container">
+//     <div class="image_div">
+//     <img id="nothingImage" src="img/nothing.gif" alt="Nothing Found"/>
+//   </div>
+
+//     <div class="card-content">
+//       <h2 class="bookTitle">Nothing found if nothing searched</h2>
+//       <p>Maybe try typing something next time to get the best books results Buckbeak can offer</p>
+//     </div>
+// </div>;`;
